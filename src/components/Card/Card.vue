e5dbe1a46e2af707b49256d8c5776627c440b317
Shruthi Reddy <shruthi.reddy@qentelli.com>
2022-02-03 10:13:40 +0530

    Home and card-list

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,31 @@
+<template>
+<div className='card-container'>
+<img
+alt='monster'
+src='https://robohash.org/${id}?set=set2&size=180x180'
+/>
+<h2> {name} </h2>
+<p> {email} </p>
+</div>
+</template>
+<script>
+export default {
+    name: 'card'    
+}
+</script>
+
+<style >
+.card-container{
+  display: flex;
+  flex-direction: column;
+  background-color: #95dada;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+  -moz-osx-font-smoothing: grayscale;
+  backface-visibility: hidden;
+  transform: translateZ(0);
+  transition: transform 0.25s ease-out;
+}
+</style>
